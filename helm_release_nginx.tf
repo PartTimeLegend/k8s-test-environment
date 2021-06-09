2f2f735ddac7949c23f146bd05034bd6ca786b28
Antony Bailey <hi@antonybailey.net>
2021-06-09 10:40:17 +0100

    Change to ref name

--- helm_release_nginx.tf
+++ helm_release_nginx.tf
@@ -2,7 +2,7 @@ resource "helm_release" "nginix_ingress" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx/"
   chart      = "ingress-nginx"
-  namespace  = "antony-test"
+  namespace  = kubernetes_namespace.test-namespace.name
   depends_on = [
     kubernetes_namespace.test-namespace
   ]
